54e97d1007e9dbfc84ac168feb449550df92cec8
Jason Witt <jawittdesigns@gmail.com>
2017-06-18 18:21:27 -0400

    Update

--- react/tut-1/ReduxSimpleStarter/src/index.js
+++ react/tut-1/ReduxSimpleStarter/src/index.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
+import SearchBar from './components/search_bar';
+
 // YouTube API Key
-// 
 const API_KEY = 'AIzaSyDsv_W6lqh3FSHt-KQSZMwlLiuGFpbNpD8';
 
 // Create a new component. This component should produce some HTML.
 const App = () => {
-	return <div>Hi!</div>;
+	return (
+		<div>
+			<SearchBar />
+		</div>
+	);
 }
 
 // Tale this component's generated HTML and put it in the page (in the DOM).
