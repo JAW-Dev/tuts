a16e4a5030cc385cae4ae0a44c27d4c966151b50
Jason Witt <jawittdesigns@gmail.com>
2017-06-25 17:56:54 -0400

    Update

--- react/tut-1/ReduxSimpleStarter/src/index.js
+++ react/tut-1/ReduxSimpleStarter/src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
@@ -14,8 +15,12 @@ class App extends Component {
 			videos: [],
 			slectedVideo: null
 		};
-
-		YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+		
+		this.videoSearch('surfboards');
+	}
+	
+	videoSearch(term) {
+		YTSearch({key: API_KEY, term: term}, (videos) => {
 			this.setState({
 				videos: videos,
 				slectedVideo: videos[0]
@@ -24,9 +29,11 @@ class App extends Component {
 	}
 
 	render() {
+		const videoSearch = _.debounce((term) => { this.videoSearch(term) }, 300);
+		
 		return (
 			<div>
-				<SearchBar />
+				<SearchBar onSearchTermChange={videoSearch}/>
 				<VideoDetail video={this.state.slectedVideo}/>
 				<VideoList
 					onVideoSelect={slectedVideo => this.setState({slectedVideo})}
