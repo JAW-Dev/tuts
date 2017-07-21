c22c06e84ed44c3c3a7c460ce4f6ad415b542ab7
Jason Witt <jawittdesigns@gmail.com>
2017-07-21 07:29:07 -0400

    Update

--- laracasts2017/app/Http/Controllers/RegistrationController.php
+++ laracasts2017/app/Http/Controllers/RegistrationController.php
@@ -2,46 +2,22 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
-use App\User;
-
-use App\Mail\Welcome;
+use App\Http\Requests\RegistrationForm;
 
 class RegistrationController extends Controller
-
 {
 	
 	public function create()
-	
 	{
-		
 		return view('registration.create');
-		
 	}
 	
-	public function store()
-	
+	public function store(RegistrationForm $form)
 	{
-		
-		$this->validate(request(), [
-		
-			'name' => 'required',
-			
-			'email' => 'required|email',
-			
-			'password' => 'required|confirmed',
-		]);
-		
-		$user = User::create(
-			request(['name', 'email', 'password'])
-		);
-		
-		auth()->login($user);
-		
-		\Mail::to($user)->send(new Welcome($user));
+		$form->persist();
 		
 		return redirect()->home();
 		
 	}
+	
 }
